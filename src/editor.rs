46055d178e06477c4d3f235c8c2497c35c9459b6
Valeriooooh <diogocsvalerio@protonmail.com>
2023-03-28 15:39:30 +0100

    fix: no more unwanted linebreak!

--- src/editor.rs
+++ src/editor.rs
@@ -3,7 +3,7 @@ mod syntax_highlighting;
 use eframe::egui;
 use rfd;
 
-use self::file::file_save;
+use self::file::file_write;
 
 pub struct Settings {
     pub font_size: u32,
@@ -14,6 +14,7 @@ pub struct Editor {
     pub picked_path: String,
     pub left_panel: bool,
     pub code: String,
+    pub saved: bool,
 }
 
 impl Default for Editor {
@@ -21,6 +22,7 @@ impl Default for Editor {
         Self {
             lang: String::from("java"),
             left_panel: false,
+            saved: false,
             picked_path: "untitled.txt".to_string(),
             code: String::from(
                 "public class Test{
@@ -45,70 +47,20 @@ impl eframe::App for Editor {
             egui::menu::bar(ui, |ui| {
                 ui.menu_button("File", |ui| {
                     if ui.button("Open").clicked() {
-                        match rfd::FileDialog::new().pick_file() {
-                            Some(path) => {
-                                let path = Some(path.display().to_string());
-                                match path {
-                                    Some(a) => {
-                                        self.picked_path = a;
-                                        match file::file_read(self.picked_path.clone()) {
-                                            Ok(a) => self.code = a,
-                                            Err(e) => println!("Error: {:?}", e),
-                                        };
-                                    }
-                                    None => {}
-                                }
-                            }
-                            _ => (),
-                        }
+                        file::file_open(self)
                     }
                     if ui.button("Save").clicked() {
                         if self.picked_path == Editor::default().picked_path {
-                            match rfd::FileDialog::new().save_file() {
-                                Some(path) => {
-                                    let path = Some(path.display().to_string());
-                                    match path {
-                                        Some(a) => {
-                                            self.picked_path = a;
-                                            match file_save(
-                                                self.picked_path.clone(),
-                                                self.code.clone(),
-                                            ) {
-                                                Ok(_) => {}
-                                                Err(e) => println!("Error: {:?}", e),
-                                            };
-                                        }
-                                        None => {}
-                                    }
-                                }
-                                _ => (),
-                            }
+                            file::file_save(self)
                         } else {
-                            match file_save(self.picked_path.clone(), self.code.clone()) {
+                            match file_write(self.picked_path.clone(), self.code.clone()) {
                                 Ok(_) => {}
                                 Err(e) => println!("Error: {:?}", e),
                             };
                         }
                     }
                     if ui.button("Save as").clicked() {
-                        match rfd::FileDialog::new().save_file() {
-                            Some(path) => {
-                                let path = Some(path.display().to_string());
-                                match path {
-                                    Some(a) => {
-                                        self.picked_path = a;
-                                        match file_save(self.picked_path.clone(), self.code.clone())
-                                        {
-                                            Ok(_) => {}
-                                            Err(e) => println!("Error: {:?}", e),
-                                        };
-                                    }
-                                    None => {}
-                                }
-                            }
-                            _ => (),
-                        }
-                        // frame.close()
+                        file::file_save(self)
                     }
                     if ui.button("Quit").clicked() {
                         frame.close()
@@ -130,13 +82,6 @@ impl eframe::App for Editor {
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            // let mut theme = syntax_highlighting::CodeTheme::from_memory(u);
-            // ui.collapsing("Theme", |ui| {
-            //     ui.group(|ui| {
-            //         theme.ui(ui);
-            //         theme.clone().store_in_memory(ui);
-            //     });
-            // });
             if self.left_panel {
                 egui::SidePanel::left("side_panel").show_inside(ui, |ui| {
                     ui.heading("Project Tree\t\t");
@@ -149,8 +94,8 @@ impl eframe::App for Editor {
                 let codeclone = &self.code.to_owned();
                 let mut layouter = |ui: &egui::Ui, _string: &str, _wrap_width: f32| {
                     let mut layout_job =
-                        syntax_highlighting::highlight(ui.ctx(), &theme, codeclone, &self.lang);
-                    layout_job.wrap.max_width = f32::INFINITY;
+                        syntax_highlighting::highlight(ui.ctx(), &theme, _string, &self.lang);
+                    layout_job.wrap.max_width = _wrap_width;
                     ui.fonts(|f| f.layout_job(layout_job))
                 };
 
--- src/editor/file.rs
+++ src/editor/file.rs
@@ -1,7 +1,11 @@
 use std::{
+    ffi::OsStr,
     fs::File,
     io::{BufReader, Read},
+    path::Path,
 };
+
+use super::Editor;
 
 #[derive(Debug)]
 pub enum FileError {
@@ -23,10 +27,61 @@ pub fn file_read(path: String) -> Result<String, FileError> {
     Ok(str)
 }
 
-pub fn file_save(path: String, content: String) -> Result<(), FileError> {
+pub fn file_write(path: String, content: String) -> Result<(), FileError> {
     match std::fs::write(path, content) {
         Ok(_) => {}
         Err(e) => return Err(FileError::WriteError(e.to_string())),
     };
     Ok(())
+}
+
+pub fn file_open(ed: &mut Editor) {
+    match rfd::FileDialog::new().pick_file() {
+        Some(path) => {
+            let prevpath = ed.picked_path.clone();
+            let path = Some(path.display().to_string());
+            match path {
+                Some(a) => {
+                    ed.picked_path = a;
+                    match file_read(ed.picked_path.clone()) {
+                        Ok(a) => {
+                            ed.code = a;
+                            ed.saved = true;
+                            let extension = match Path::new(ed.picked_path.as_str())
+                                .extension()
+                                .and_then(OsStr::to_str)
+                            {
+                                Some(a) => a.to_string(),
+                                None => ed.lang.clone(),
+                            };
+                            ed.lang = extension;
+                        }
+                        Err(e) => {
+                            ed.picked_path = prevpath;
+                            println!("Error: {:?}", e);
+                        }
+                    };
+                }
+                None => {}
+            }
+        }
+        _ => (),
+    }
+}
+pub fn file_save(ed: &mut Editor) {
+    match rfd::FileDialog::new().save_file() {
+        Some(path) => {
+            let path = Some(path.display().to_string());
+            match path {
+                Some(a) => {
+                    match file_write(ed.picked_path.clone(), ed.code.clone()) {
+                        Ok(_) => ed.picked_path = a,
+                        Err(e) => println!("Error: {:?}", e),
+                    };
+                }
+                None => {}
+            }
+        }
+        _ => (),
+    }
 }
